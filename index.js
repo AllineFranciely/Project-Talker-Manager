d6732ffa972cc0438f3d87b74ac7a63bd017fa1b
Alline <alline_franciely@hotmail.com>
2022-06-15 19:01:55 -0300

    requisito 2

--- index.js
+++ index.js
@@ -17,6 +17,18 @@ app.get('/talker', async (_req, res, _next) => {
   } res.status(HTTP_OK_STATUS).json(talkers);
 });
 
+// REQUISITO 2:
+
+app.get('/talker/:id', async (req, res, _next) => {
+  const { id } = req.params;
+  const file = await fs.readFile('./talker.json', 'utf-8');
+  const talkers = JSON.parse(file);
+  const chosenTalker = talkers.find((talker) => talker.id === parseInt(id, 10));
+  if (!chosenTalker) {
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  } res.status(HTTP_OK_STATUS).json(chosenTalker);
+});
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
